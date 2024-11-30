88d2b99b2d5c2881db3a52ff09048bdf8af52446
Ezequeil Murga OPereyra <exekyel.murga.pereyra@gmail.com>
2024-11-30 16:53:00 -0300

    astro.config.mjs modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    // ...
-    output: 'server',
-    adapter: vercel(),
-    integrations: [tailwind(), react()],
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel()
 });
