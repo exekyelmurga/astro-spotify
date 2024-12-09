bd3833e70ed9d3fa1c7880efff8c081e1aebe9e6
Ezequeil Murga OPereyra <exekyel.murga.pereyra@gmail.com>
2024-12-09 15:13:35 -0300

    Add playerStore to src/store, create global state

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,9 +1,28 @@
 import { Play, Pause} from './PlayerSection'
+import { usePlayerStore } from '@/store/playerStore'
+
 
 export function CardPlayButton ({ id }) {
+    const { 
+        currentMusic,
+        isPlaying,
+        setIsPlaying,
+        setCurrentMusic 
+    } = usePlayerStore(state => state) 
+
+    
+    const handleClick = () => {
+        setIsPlaying(!isPlaying)
+        setCurrentMusic({ 
+            playlist: {id}
+        }) 
+    }
+    
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+    
     return (
-        <div className='rounded-full bg-green-500 p-2'>
-            <Play />
-        </div>
+        <button onClick={handleClick} className='rounded-full bg-green-500 p-2'>
+            {isPlayingPlaylist? <Pause/> :<Play />}
+        </button>
     )
 }
